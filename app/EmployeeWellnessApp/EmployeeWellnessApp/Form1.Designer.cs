f1b9828be393fa37a0f10f27884f56b704a6cf93
George <george.afilipoae>
2024-11-08 21:14:16 +0200

    login

--- app/EmployeeWellnessApp/EmployeeWellnessApp/Form1.Designer.cs
+++ app/EmployeeWellnessApp/EmployeeWellnessApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EmployeeWellnessApp
 {
-    partial class Form1
+    partial class LoginPage
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_close = new System.Windows.Forms.Button();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.Login_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_close
+            // 
+            this.button_close.Location = new System.Drawing.Point(1351, 55);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(63, 37);
+            this.button_close.TabIndex = 0;
+            this.button_close.Text = "X";
+            this.button_close.UseVisualStyleBackColor = true;
+            this.button_close.Click += new System.EventHandler(this.button_close_Click);
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(549, 614);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(369, 29);
+            this.username.TabIndex = 1;
+            this.username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.username.TextChanged += new System.EventHandler(this.username_TextChanged);
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(549, 700);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(369, 29);
+            this.password.TabIndex = 2;
+            this.password.TextChanged += new System.EventHandler(this.password_TextChanged);
+            // 
+            // Login_button
+            // 
+            this.Login_button.Location = new System.Drawing.Point(669, 758);
+            this.Login_button.Name = "Login_button";
+            this.Login_button.Size = new System.Drawing.Size(127, 37);
+            this.Login_button.TabIndex = 3;
+            this.Login_button.Text = "Login";
+            this.Login_button.UseVisualStyleBackColor = true;
+            this.Login_button.Click += new System.EventHandler(this.Login_button_Click);
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(231)))), ((int)(((byte)(249)))));
+            this.ClientSize = new System.Drawing.Size(1896, 1016);
+            this.Controls.Add(this.Login_button);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.button_close);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginPage";
+            this.RightToLeftLayout = true;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_close;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button Login_button;
     }
 }
 

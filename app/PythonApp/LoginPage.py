4b5eac5bd427d28b1e135eb641cf7f3711aac358
George <george.afilipoae>
2024-11-09 10:31:58 +0200

    buna2

--- app/PythonApp/LoginPage.py
+++ app/PythonApp/LoginPage.py
@@ -52,36 +52,49 @@ def add_user():
             
 # Create main window
 root = tk.Tk()
-root.title("Login App")
-root.geometry("400x250")
-root.configure(bg="#f0f0f0")  # Light background color
+root.title("Wellness App")
+root.geometry("1920x1080")
+root.configure(bg="#B8BCD3")  # Light background color
 
 # Create a frame for the login form
-login_frame = ttk.Frame(root, padding=20)
-login_frame.place(relx=0.5, rely=0.5, anchor="center")  # Center the frame
+# login_frame = ttk.Frame(root, padding=20)
+# login_frame.place(relx=0.5, rely=0.5, anchor="center")  # Center the frame
 
 # Add a title label
-title_label = ttk.Label(login_frame, text="Login", font=("Helvetica", 18, "bold"))
-title_label.grid(row=0, column=0, columnspan=2, pady=10)
+welcome_text = "Welcome!\nAre you ready to conquer the day? 😊"
+title_label = ttk.Label(root, text=welcome_text,anchor="center",font=("Helvetica",35,"bold"),background="#B8BCD3",justify="center")
+title_label.grid(row=0, column=0, columnspan=1, pady=10)
+title_label.place(relx=0.5, rely=0.3, width=1200, height=150, anchor="center")
 
 # Create and place username label and entry
-username_label = ttk.Label(login_frame, text="Username:")
+username_label = ttk.Label(root, text="Username:",anchor="center",font=("Helvetica",16,"bold"),background="#B8BCD3")
 username_label.grid(row=1, column=0, sticky="w", pady=5)
-username_entry = ttk.Entry(login_frame, width=25)
+username_label.place(relx=0.435,rely=0.4,width=170,height=35,anchor="center")
+
+username_entry = tk.Entry(root, width=25,font=("Helvetica",16),bg="#E1E3F0")
 username_entry.grid(row=1, column=1, pady=5)
+username_entry.place(relx=0.535,rely=0.4,width=320,height=35,anchor="center")
 
 # Create and place password label and entry
-password_label = ttk.Label(login_frame, text="Password:")
+password_label = ttk.Label(root, text="Password:",anchor="center",font=("Helvetica",16,"bold"),background="#B8BCD3")
 password_label.grid(row=2, column=0, sticky="w", pady=5)
-password_entry = ttk.Entry(login_frame, show="*", width=25)
+password_label.place(relx=0.435,rely=0.45,width=170,height=35,anchor="center")
+
+password_entry = tk.Entry(root, show="*", width=25,font=("Helvetica",16),bg="#E1E3F0")
 password_entry.grid(row=2, column=1, pady=5)
+password_entry.place(relx=0.535,rely=0.45,width=320,height=35,anchor="center")
+
+style = ttk.Style()
+style.configure("TButton", font=("Helvetica", 16, "bold"),foreground="#7885C5",borderwidth=2)
 
 # Create and place login button
-login_button = ttk.Button(login_frame, text="Login", command=validate_login)
+login_button = ttk.Button(root, text="Login", command=validate_login,style="TButton")
 login_button.grid(row=3, column=0, columnspan=2, pady=20)
+login_button.place(relx=0.445,rely=0.5,width=250,height=60,anchor="center")
 
-auth_button = ttk.Button(login_frame, text="Auth", command=add_user)
+auth_button = ttk.Button(root, text="Create new account", command=add_user,style="TButton")
 auth_button.grid(row=4, column=0, columnspan=2, pady=20)
+auth_button.place(relx=0.548,rely=0.5,width=260,height=60,anchor="center")
 
 # Start the main event loop
 root.mainloop()
